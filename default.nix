94499eaa5f9a447ad559c6fbbb7163ce15c3af31
ChanceCtrl <StaticElite05@gmail.com>
2024-02-24 17:48:38 +0000

    idk man
    maybe this is the bug???

--- default.nix
+++ default.nix
@@ -6,10 +6,6 @@ pkgs.stdenv.mkDerivation rec {
   src = ./DBC_out;
   
   # buildInputs = [ python311Packages.cantools ]; # Python as a build dependency
-  
-  # Define the build phase to execute the scripts
-  buildPhase = ''
-  '';
 
   # Specify the output of the build process
   # In this case, it will be the generated file
